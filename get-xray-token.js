6e5455b9bd8762012fefdfe55415ba05abc14268
Fodé NDIAYE <fndiaye@agiltoo.fr>
2024-09-07 04:03:20 +0200

    modif yaml file

---
+++ get-xray-token.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+async function getXrayToken() {
+  const clientId = process.env.XRAY_CLIENT_ID;
+  const clientSecret = process.env.XRAY_CLIENT_SECRET;
+
+  try {
+    const response = await axios.post(
+      "https://xray.cloud.xpand-it.com/api/v1/authenticate",
+      {
+        client_id: clientId,
+        client_secret: clientSecret,
+      }
+    );
+    return response.data; // Le token est renvoyé en tant que chaîne
+  } catch (error) {
+    console.error("Failed to get Xray token:", error);
+    process.exit(1);
+  }
+}
+
+getXrayToken().then((token) => {
+  console.log(`::add-mask::${token}`); // Masquer le token dans les logs GitHub Actions
+  console.log(`XRAY_API_TOKEN=${token}`); // Imprimer le token pour l'utiliser dans les étapes suivantes
+});
--- send-results.js
+++ send-results.js
@@ -1,6 +1,15 @@
 const axios = require("axios");
 const fs = require("fs");
 
+// Récupérer le token Xray depuis les variables d'environnement
+const xrayApiToken = process.env.XRAY_API_TOKEN;
+
+if (!xrayApiToken) {
+  console.error('XRAY_API_TOKEN is not set');
+  process.exit(1); // Quitter avec une erreur si le token n'est pas défini
+}
+
+// Fonction pour envoyer les résultats à Xray
 async function sendResults() {
   const results = fs.readFileSync("xray-report.json", "utf8");
   const response = await axios.post(
@@ -9,8 +18,7 @@ async function sendResults() {
     {
       headers: {
         "Content-Type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnQiOiIzZjY0MzVkNi02MWI4LTNiM2MtYWVjNi1iZGJhZDcwMzM1ZmUiLCJhY2NvdW50SWQiOiI2MWRkYWZmNDEyNWIxMjAwNzE0MjI0NmYiLCJpc1hlYSI6ZmFsc2UsImlhdCI6MTcyNTExOTE1NiwiZXhwIjoxNzI1MjA1NTU2LCJhdWQiOiJCNTRFNDYzODlGM0Q0RkE3OTk0NjMwMDA0NkI3RTAwOSIsImlzcyI6ImNvbS54cGFuZGl0LnBsdWdpbnMueHJheSIsInN1YiI6IkI1NEU0NjM4OUYzRDRGQTc5OTQ2MzAwMDQ2QjdFMDA5In0.79acK4gz8LVHHRNLXIUxc_zT-pkUVIxduCuNb0BTeTk",
+        Authorization: `Bearer ${xrayApiToken}`, // Utilisation du token
       },
     }
   );
